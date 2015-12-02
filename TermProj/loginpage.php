547ed8cd5709d6d55822e04ac5c7e7279f3ca2b6
Graigor Pierre-Noel <gpnoel111@yahoo.com>
2015-12-02 16:04:00 -0500

    removing drop-down menu
    removed sign-in dropdown menu on loginpage.php

--- TermProj/loginpage.php
+++ TermProj/loginpage.php
@@ -48,22 +48,6 @@
                 </button>
                 <a class="navbar-brand" href="index.html">StudyBuddy+</a>
             </div>
-            <!-- Top Menu Items -->
-            <ul class="nav navbar-right top-nav">
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> Sign in! <b
-                            class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li>
-                            <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-fw fa-gear"></i> Settings</a>
-                        </li>
-                        <li class="divider"></li>
-                    </ul>
-                </li>
-            </ul>
         </nav>
 
         <div id="wrapper">
