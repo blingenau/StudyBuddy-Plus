dd81a5d26df6c67ec808aec33673ef2bdd9543cb
Graigor Pierre-Noel <gpnoel111@yahoo.com>
2015-12-02 19:16:16 -0500

    deletion

--- TermProj/dashboard.php
+++ TermProj/dashboard.php
@@ -203,36 +203,6 @@
                     </div>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6">
-                <div class="panel panel-yellow">
-                    <div class="panel-heading">
-                        <div class="row">
-                            <div class="col-xs-3">
-                                <i class="fa fa-book fa-5x"></i>
-                            </div>
-                            <div class="col-xs-9 text-right">
-                                <div class="huge">13</div>
-                                <div>Assignments due this week!</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <div class="panel panel-red">
-                    <div class="panel-heading">
-                        <div class="row">
-                            <div class="col-xs-3">
-                                <i class="fa fa-pencil-square fa-5x"></i>
-                            </div>
-                            <div class="col-xs-9 text-right">
-                                <div class="huge">5</div>
-                                <div>Tests the week!</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
         <!-- /.row -->
 
@@ -283,89 +253,19 @@
                         <h3 class="panel-title"><i class="fa fa-clock-o fa-fw"></i>Group Searcher</h3>
                     </div>
                     <div class="panel-body">
-                        <form action="grouppage.php" method="post">
+                        <form action="dashboard.php" method="post">
                             <div class="list-group">
-                                <input style="display:none" name="gname" type="text" value="<?php echo $group?>"/>
-                                <input style="width:80%" name="comment" type="text" placeholder="enter the group name here"/>
+                                Search by group name or course number
+                                <input style="width:80%" name="gName" type="text" placeholder="enter group name"/>
+                                <input style="width:80%" name="cNum" type="text" placeholder="enter course number"/>
                             </div>
                             <div class="text-right">
                                 <input name="send" type="submit" value="search"/>
                             </div>
+                            <?php 
+                                
+                            ?>
                         </form>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-4">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h3 class="panel-title"><i class="fa fa-money fa-fw"></i>CLASS PLACEHOLDER</h3>
-                    </div>
-                    <div class="panel-body">
-                        <div class="table-responsive">
-                            <table class="table table-bordered table-hover table-striped">
-                                <thead>
-                                <tr>
-                                    <th>Order #</th>
-                                    <th>Order Date</th>
-                                    <th>Order Time</th>
-                                    <th>Amount (USD)</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr>
-                                    <td>3326</td>
-                                    <td>10/21/2013</td>
-                                    <td>3:29 PM</td>
-                                    <td>$321.33</td>
-                                </tr>
-                                <tr>
-                                    <td>3325</td>
-                                    <td>10/21/2013</td>
-                                    <td>3:20 PM</td>
-                                    <td>$234.34</td>
-                                </tr>
-                                <tr>
-                                    <td>3324</td>
-                                    <td>10/21/2013</td>
-                                    <td>3:03 PM</td>
-                                    <td>$724.17</td>
-                                </tr>
-                                <tr>
-                                    <td>3323</td>
-                                    <td>10/21/2013</td>
-                                    <td>3:00 PM</td>
-                                    <td>$23.71</td>
-                                </tr>
-                                <tr>
-                                    <td>3322</td>
-                                    <td>10/21/2013</td>
-                                    <td>2:49 PM</td>
-                                    <td>$8345.23</td>
-                                </tr>
-                                <tr>
-                                    <td>3321</td>
-                                    <td>10/21/2013</td>
-                                    <td>2:23 PM</td>
-                                    <td>$245.12</td>
-                                </tr>
-                                <tr>
-                                    <td>3320</td>
-                                    <td>10/21/2013</td>
-                                    <td>2:15 PM</td>
-                                    <td>$5663.54</td>
-                                </tr>
-                                <tr>
-                                    <td>3319</td>
-                                    <td>10/21/2013</td>
-                                    <td>2:13 PM</td>
-                                    <td>$943.45</td>
-                                </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                        <div class="text-right">
-                            <a href="#">View All Transactions <i class="fa fa-arrow-circle-right"></i></a>
-                        </div>
                     </div>
                 </div>
             </div>
